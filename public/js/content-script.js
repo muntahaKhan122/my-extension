9212bce0e6bb256d216cb1f696e5888887d538e1
Muntaha Khan <kmuntaha122@gmail.com>
2021-08-20 12:19:25 +0500

    commit on master

--- public/js/content-script.js
+++ public/js/content-script.js
@@ -15,7 +15,7 @@ var notes;
 async function getNotes(){
  notes=await readLocalStorage('notes');
 
-const wrapperDiv = document.createElement("div");
+const wrapperDiv= document.createElement("div");
 wrapperDiv.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
 const iframeElement = document.createElement("iframe");
 iframeElement.setAttribute("style","width: 100%; height: 100%;");
